bb92edd6d43fbc0b6c8851b9bc337e8f7bd7230e
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-01-22 16:13:45 +0300

    Added modles and logic for backend part

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :todos
+  post '/refresh', to: 'refresh#create'
+  post '/signin', to: 'signin#create'
+  post '/signup', to: 'signup#create'
+  delete '/signin', to: 'signin#destroy'
 end
