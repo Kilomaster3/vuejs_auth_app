6e0381886c2d1ee9f25df5fd34fa87fd16f7f75c
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-01-24 20:32:45 +0300

    Refactor tests for rspec

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo do
-    title { "MyString" }
+    title { 'MyString' }
     user { nil }
   end
 end
--- spec/requests/refresh_request_spec.rb
+++ spec/requests/refresh_request_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Refreshes", type: :request do
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/refresh/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
