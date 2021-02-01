148c0aa86ec061f9f6f447682f743f91fc1465df
!kVbP3#BiC%V <grom.4lek@yandex.by>
2021-02-01 23:21:47 +0300

    Fix bug with SignIn and SignUp form

--- todos-vue/src/router/index.js
+++ todos-vue/src/router/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin'
 import Signup from '@/components/Signup'
-import TodosList from '@/components/todos/List'
-import UsersList from '@/components/admin/users/List'
-import UserTodosList from '@/components/admin/users/todos/List'
 import ForgotPassword from '@/components/ForgotPassword'
 import ResetPassword from '@/components/ResetPassword'
+import TodosList from '@/components/todos/List'
+import UsersList from '@/components/admin/users/List'
 import UserEdit from '@/components/admin/users/Edit'
+import UserTodosList from '@/components/admin/users/todos/List'
 
 Vue.use(Router)
 
@@ -29,16 +29,6 @@ export default new Router({
       component: TodosList
     },
     {
-      path: '/admin/users',
-      name: 'UsersList',
-      component: UsersList
-    },
-    {
-      path: '/admin/users/:id/todos',
-      name: 'UserTodosList',
-      component: UserTodosList
-    },
-    {
       path: '/forgot_password',
       name: 'ForgotPassword',
       component: ForgotPassword
@@ -47,6 +37,16 @@ export default new Router({
       path: '/password_resets/:token',
       name: 'ResetPassword',
       component: ResetPassword
+    },
+    {
+      path: '/admin/users',
+      name: 'UsersList',
+      component: UsersList
+    },
+    {
+      path: '/admin/users/:id/todos',
+      name: 'UserTodosList',
+      component: UserTodosList
     },
     {
       path: '/admin/users/:id',
